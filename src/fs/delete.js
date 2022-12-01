17482a1db9d83b978b5811c1b24fe72b135a4f61
Zankorrr <zankor92@gmail.com>
2022-12-01 21:26:06 +0300

    feat: fs-delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import { rm } from 'node:fs/promises'
+import path from 'node:path';
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        const filePath = new URL(path.join('files', 'fileToRemove.txt'), import.meta.url)
+        await rm(filePath)
+    } catch(err) {
+        const error = new Error('FS operation failed')
+        console.log(error)
+    }
 };
 
 await remove();
