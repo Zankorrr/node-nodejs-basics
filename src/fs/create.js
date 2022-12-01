68533499dbaa8d990977f72d7f0e213b3f468fbf
Zankorrr <zankor92@gmail.com>
2022-12-01 12:12:03 +0300

    feat: fs-create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const content = 'I am fresh and young'
+        const filePath = new URL(path.join('files', 'fresh.txt'), import.meta.url)
+        await writeFile(filePath, content, { flag: 'ax' })
+    } catch (err) {
+        const error = new Error('FS operation failed')
+        console.log(error)
+    }
 };
 
 await create();
