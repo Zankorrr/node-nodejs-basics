175fd7fa314a908b51207f12fa25d2ea1909380c
Zankorrr <zankor92@gmail.com>
2022-12-03 12:35:19 +0300

    feat: cli-args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.reduce((acc, val, index, arr) => {
+        if(val.startsWith('--')) {
+            const pair = `${val.slice(2)} is ${arr[index+1]}`
+            acc.push(pair)
+        }
+        return acc
+    }, [])
+    console.log(args.join(", "))
 };
 
 parseArgs();
