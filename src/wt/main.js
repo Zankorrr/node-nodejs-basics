e67ee3b151d435f01355f3db44be6b456d4d34e4
Zankorrr <zankor92@gmail.com>
2022-12-03 15:39:06 +0300

    feat: wt-worker.js, wt-main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import { Worker } from 'node:worker_threads'
+import { cpus } from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const cores = cpus()
+    let number = 10
+
+    const workerResults = await Promise.allSettled(cores.map(() => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(new URL('./worker.js', import.meta.url), {
+                workerData: number++
+            });
+            worker.on('message', resolve)
+            worker.on('error', reject)
+        })
+    }))
+
+    const results = workerResults.map(({status, value}) => ({
+        status: status === 'fulfilled' ? 'resolved' : 'error',
+        data: status === 'fulfilled' ? value : null
+    }))
+
+    console.log(results)
 };
 
 await performCalculations();
