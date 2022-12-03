0bbd909fcf18a22b855b76fb054857ce803f5d80
Zankorrr <zankor92@gmail.com>
2022-12-03 12:06:30 +0300

    feat: cli-env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rss = Object.entries(process.env).reduce((acc, val) => {
+        if (val[0].startsWith('RSS_')) {
+            acc.push(`${val[0]}=${val[1]}`)
+        }
+        return acc
+    }, [])
+    const str = rss.join('; ')
+    console.log(str)
 };
 
 parseEnv();
