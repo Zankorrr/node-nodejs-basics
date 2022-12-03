1060bdb89a70ffb57a756ebabf69862b689e2a1f
Zankorrr <zankor92@gmail.com>
2022-12-03 14:33:20 +0300

    feat: zip-compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,11 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib'
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip()
+    const source = createReadStream(new URL('./files/fileToCompress.txt', import.meta.url))
+    const destination = createWriteStream(new URL('./files/archive.gz', import.meta.url))
+    source.pipe(gzip).pipe(destination)
 };
 
 await compress();
